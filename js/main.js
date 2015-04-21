e2dbad11d396ac5a5036652b02b0323ddfac34e2
hecnabae <hecnabae@gmail.com>
2015-04-21 21:57:37 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+'use strict';
+angular.module('videogularApp', [
+			"ngSanitize",
+			"com.2fdevs.videogular",
+			"com.2fdevs.videogular.plugins.controls",
+			"com.2fdevs.videogular.plugins.overlayplay",
+			"com.2fdevs.videogular.plugins.poster"
+		])
+	.controller('videogularCtrl', ["$sce", function ($sce) {
+		var controller = this;
+
+		controller.config = {
+			preload: "none",
+			sources: [
+				{
+					src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"),
+					type: "video/mp4"
+				},
+				{
+					src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"),
+					type: "video/webm"
+				},
+				{
+					src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"),
+					type: "video/ogg"
+				}
+				],
+			theme: "http://www.videogular.com/styles/themes/default/latest/videogular.css",
+			plugins: {
+				poster: "http://www.videogular.com/assets/images/videogular.png"
+			}
+		};
+		}])
+	.directive("vgQuizPlugin", [function () {
+			return {
+				restrict: "E",
+				require: "^videogular",
+				template: "<div ng-show=\"API.isCompleted && API.currentState == 'stop'\"><span ng-click=\"onClickReplay()\">REPLAY!</span></div>",
+				link: function (scope, elem, attrs, API) {
+					scope.API = API;
+
+					scope.onClickReplay = function () {
+						API.play();
+					};
+				}
+			}
+		}
+	]);
