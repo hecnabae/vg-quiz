bf09ed759fea184a205e9c062c9578746832caeb
hecnabae <hecnabae@gmail.com>
2015-04-23 20:39:57 +0200

    Solve some bugs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict';
-angular.module('videogularApp', [
+angular.module("videogularApp", [
 			"ngSanitize",
 			"com.2fdevs.videogular",
 			"com.2fdevs.videogular.plugins.controls",
 			"com.2fdevs.videogular.plugins.overlayplay",
 			"com.2fdevs.videogular.plugins.poster"
 		])
-	.controller('VideogularCtrl', ["$scope", function ($scope) {
+	.controller("VideogularCtrl", ["$scope", "$sce", function ($scope, $sce) {
 		this.API = null;
 
 		this.onPlayerReady = function onPlayerReady(API) {
@@ -19,15 +19,13 @@ angular.module('videogularApp', [
 			var quiz = {};
 			quiz.timeLapse = {
 				start: 10,
-				end: 10
+				end: 15
 			};
 			quiz.onLeave = this.onLeave.bind(this);
 			quiz.onUpdate = this.onUpdate.bind(this);
 			quiz.onComplete = this.onComplete.bind(this);
 
 			quizCollection.push(quiz);
-
-
 
 			this.config = {
 				preload: "none",
@@ -53,27 +51,48 @@ angular.module('videogularApp', [
 					poster: "http://www.videogular.com/assets/images/videogular.png"
 				}
 			};
+		};
+		this.onLeave = function onLeave(currentTime, timeLapse, params) {
 
-			this.onLeave = function onLeave(currentTime, timeLapse, params) {
-
-			};
-
-			this.onComplete = function onComplete(currentTime, timeLapse, params) {
-				// TODO: Este evento en realidad no tiene mucha funcionalidad
-			};
-
-			this.onUpdate = function onUpdate(currentTime, timeLapse, params) {
-				API.pause();
-				// TODO: Mostrar quizz
-
-			};
-
-			this.init();
 		};
 
+		this.onComplete = function onComplete(currentTime, timeLapse, params) {
+			// TODO: Este evento en realidad no tiene mucha funcionalidad
+			API.pause();
+		};
+
+		this.onUpdate = function onUpdate(currentTime, timeLapse, params) {
+			API.pause();
+			// TODO: Mostrar quizz
+
+		};
+		this.init();
 
 
 		}]);
+//angular.module("videogularApp")
+//    .directive("vgQuizPlugin",
+//        function vgQuizPlugin() {
+//            return {
+//				restrict: "E",
+//				require: "^videogular",
+//				controller: "VideogularCtrl",
+//				controllerAs: "ctrl",
+//				template: "<div ng-show=\"API.isCompleted && API.currentState == 'stop'\"><span ng-click=\"onClickReplay()\">REPLAY!</span></div>",
+//				link: function link(scope, elem, attrs, ctrl) {
+//					//ctrl.tweetElements = elem[0].getElementsByTagName("vg-tweet");
+//					ctrl.quizElements = elem[0].getElementsByTagName("vq-quiz-plugin");
+//				}
+//            };
+//        }
+//    );
+
+
+
+
+
+
+
 //	.directive("vgQuizPlugin", [function () {
 //			return {
 //				restrict: "E",
